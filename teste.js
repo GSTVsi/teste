34030a7c3b3111b0e6c7c37f373873747f192375
Gustavo Silva <gustavo.p.lucas1000@gmail.com>
2020-10-20 11:22:28 -0300

    teste

---
+++ teste.js
@@ -0,0 +1,9 @@
+// comentario da linha
+console.log('linha 1');
+/* 
+ * comentarios com mais
+ * de uma linha
+ * Forma de comentário padrão utilizada.
+ * Sempre formar comentarios relevantes.
+ */
+console.log('Linha 2 do teste de comentario');
